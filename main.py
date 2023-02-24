0f13b972cd370b38869cb30c03087a3b64be88b7
Murilo <124837468+asm-murilo@users.noreply.github.com>
2023-02-24 11:09:18 -0400

    Corrigido erros ortográficos

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/bin/env python3
 
 # Multiple Strings Tester :: MUSTR-tester
-# O funcionamento deste script é bem simples. Para o funcionamento do script é nesscesarrio dois argumantos, o nome do arquivo que contem
+# O funcionamento deste script é bem simples. Para o funcionamento do script é nesscesarrio dois argumentos, o nome do arquivo que contem
 # o texto, que logo sera transformado em um array, onde o fim de um elemento é uma quebra de linha (\n), assim cada linha do texto é um elemento
 # no array. E a frase para testar.
 #
-# De uma forma mais simples, o script testa uma certa frase em um conjunto de frases, se uma frase desses conjuntos de frases for diferente do frase que está testando,
+# De uma forma mais simples, o script testa uma certa frase em um conjunto de frases, se uma frase desses conjuntos de frases for diferente da frase que está testando,
 # o script encerra a verificação e mostra a o usuário a frase diferente e a linha que essa frase está.
 
 from sys import argv
